f84891264f84934ac469c87b222e1fbf9cf6fe8f
Muhammad Shamil k k <shamilmuhammadsha@gmail.com>
2023-05-21 02:39:39 +0530

    gui created

---
+++ youtubeDownloader.py
@@ -0,0 +1,19 @@
+from pytube import YouTube
+from tkinter import*
+window=Tk()
+window.title("YOUTUBE VIDEO DOWNLOADER")
+window.geometry("600x400")
+window.iconbitmap("images/YouTube_23392.ico")
+window.configure(bg="#d9faf1")
+
+
+
+
+
+
+
+
+
+window.mainloop()
+#gui created
+
